b2b60f764a76018cedb4f212c0517481c91f8601
Icep0ps <tapsmuko@gmail.com>
2023-11-06 00:01:51 +0200

    added thank message to contact and fixed responsive for about

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,5 +1,10 @@
+const message = document.getElementById('thank_you');
+const form = document.getElementsByTagName('form')[0];
 const humburger = document.getElementById('humburger');
+const popup_wrapper = document.getElementById('wrapper');
+
 const humburger_icon = document.getElementById('humburger_icon');
+const close_popup_btn = document.getElementById('close_popup_btn');
 const close_humburger_icon = document.getElementById('close_humburger_icon');
 
 humburger_icon.addEventListener('click', (event) => {
@@ -7,6 +12,15 @@ humburger_icon.addEventListener('click', (event) => {
 });
 
 close_humburger_icon.addEventListener('click', (event) => {
-  console.log(event.currentTarget);
   humburger.style.display = 'none';
 });
+
+function submit(event) {
+  event.preventDefault();
+  close_popup_btn.addEventListener('click', () => {
+    popup_wrapper.style.display = 'none';
+  });
+  popup_wrapper.style.display = 'flex';
+}
+
+form.addEventListener('submit', submit, false);
